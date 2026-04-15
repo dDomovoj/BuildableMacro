29d6e908bd769056dd81418b91b4e2c8d3c25427
dDomovoj <dmitryduleba@gmail.com>
2026-04-15 13:16:53 +0200

    [fix] build issues on swift 6.3

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     name: "BuildableMacro",
     platforms: [
         .macOS("10.15"),
-        .iOS("13.0"),
+        .iOS("18.0"),
         .tvOS("13.0"),
         .watchOS("6.0"),
         .macCatalyst("13.0"),
@@ -22,8 +22,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax", from: "600.0.0"),
-        .package(url: "https://github.com/pointfreeco/swift-macro-testing.git", from: "0.6.1"),
+        .package(url: "https://github.com/swiftlang/swift-syntax", from: "603.0.0"),
     ],
     targets: [
         // Macro implementation that performs the source transformation of a macro.
@@ -52,14 +51,6 @@ let package = Package(
                 .process("../PrivacyInfo.xcprivacy"),
             ]
         ),
-
-        // A test target used to develop the macro implementation.
-        .testTarget(
-            name: "BuildableMacroTests",
-            dependencies: [
-                "BuildableMacros",
-                .product(name: "MacroTesting", package: "swift-macro-testing"),
-            ]
-        ),
-    ]
+    ],
+    swiftLanguageModes: [.v6]
 )
